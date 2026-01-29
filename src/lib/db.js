fd25ce57931382bdb30ebcadb3031ab4275076b3
harshit singh <harshitsingh8214@gmail.com>
2026-01-29 05:56:49 +0530

    inngest changes

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,6 +3,9 @@ import {ENV} from "./env.js";
 
 export const connectDB = async()=>{
     try {
+        if(!ENV.DB_URL){
+            throw new Error("DB_URL IS NOT DEFINED IN THE ENV VARIABLE")
+        }
         // console.log(ENV.DB_URL);
         const conn = await mongoose.connect(ENV.DB_URL);
         console.log("✅ connected to the Database",conn.connection.host);
--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
-import express from "express"
-import { ENV } from "./lib/env.js"
-import { connectDB } from "./lib/db.js"
+import express from "express";
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import { serve } from "inngest/express";
 
 const app = express()
-console.log(ENV.PORT)
+// console.log(ENV.PORT)
+
+app.use(express.json())
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }))
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/health", (req, res) => {
     res.status(200).json({ msg: "api is up and running" })
